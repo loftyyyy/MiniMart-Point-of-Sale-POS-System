811d9144c3f9a2805d8a64f94d79d02ae71313b3
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-19 00:47:58 +0800

    Created a test print

---
+++ POS.asm
@@ -0,0 +1,26 @@
+include C:\masm32\include\masm32rt.inc
+
+.data
+
+    name_prompt db "What's your name?", 0
+    string db 50 dup(?)
+    name_result db "Your name is: ", 0
+
+
+.code
+
+start:
+
+    push offset name_prompt
+    call StdOut
+
+    push 50
+    push offset string
+    call StdIn
+
+    push offset name_result 
+    call StdOut
+    push offset string
+    call StdOut
+
+end start
