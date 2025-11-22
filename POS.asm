63fb8d84858822f566b2edf577eb9defa15b44ce
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-22 20:07:45 +0800

    Added more items

--- POS.asm
+++ POS.asm
@@ -39,18 +39,18 @@ include C:\masm32\include\masm32rt.inc
     
     
     ; ==== Menu Text ====
-textMenu db "========= MiniMart POS System =========", 13,10,13,10,
-           "1. Coffee      - ₱39", 13,10,
-           "2. Donut       - ₱12", 13,10,
-           "3. Sandwich    - ₱15", 13,10,
-           "4. Milk        - ₱50", 13,10,
-           "5. Bread       - ₱25", 13,10,
-           "6. Chips       - ₱30", 13,10,
-           "7. Soda        - ₱20", 13,10,
-           "8. Juice       - ₱15", 13,10,
-           "9. Candy       - ₱5", 13,10,
-           "10. Egg        - ₱8", 13,10,13,10,
-           "Selection [1-10]: ", 0
+    textMenu db "========= MiniMart POS System =========", 13,10,13,10
+             db  "1. Coffee      - ₱39", 13,10
+             db  "2. Donut       - ₱12", 13,10
+             db  "3. Sandwich    - ₱15", 13,10
+             db  "4. Milk        - ₱50", 13,10
+             db  "5. Bread       - ₱25", 13,10
+             db  "6. Chips       - ₱30", 13,10
+             db  "7. Soda        - ₱20", 13,10
+             db  "8. Juice       - ₱15", 13,10
+             db  "9. Candy       - ₱5", 13,10
+             db  "10. Egg        - ₱8", 13,10,13,10
+             db  "Selection [1-10]: ", 0
 
     qtyPrompt db "Enter Quantity: ", 0
     anotherMsg db "Add another item? (Y/N): ",0 
@@ -59,7 +59,18 @@ textMenu db "========= MiniMart POS System =========", 13,10,13,10,
     ; ==== Receipt Messages ====
     receiptHdr db 13,10, "========= RECEIPT =========",0
     itemText db "Item ",0
-    ;itemNames db 
+    itemNames db "Coffee", 0,0,0,0     ; 6 chars + 4 nulls = 10 bytes (Index 0)
+              db "Donut", 0,0,0,0,0    ; 5 chars + 5 nulls = 10 bytes (Index 1)
+              db "Sandwich", 0,0       ; 8 chars + 2 nulls = 10 bytes (Index 2)
+              db "Milk", 0,0,0,0,0,0   ; 4 chars + 6 nulls = 10 bytes (Index 3)
+              db "Bread", 0,0,0,0,0    ; 5 chars + 5 nulls = 10 bytes (Index 4)
+              db "Chips", 0,0,0,0,0    ; 5 chars + 5 nulls = 10 bytes (Index 5)
+              db "Soda", 0,0,0,0,0,0   ; 4 chars + 6 nulls = 10 bytes (Index 6)
+              db "Juice", 0,0,0,0,0    ; 5 chars + 5 nulls = 10 bytes (Index 7)
+              db "Candy", 0,0,0,0,0    ; 5 chars + 5 nulls = 10 bytes (Index 8)
+              db "Egg", 0,0,0,0,0,0    ; 3 chars + 7 nulls = 10 bytes (Index 9)
+    priceText db " x ", 0
+    atText    db " @ ₱ ", 0
 
 
     ;input
