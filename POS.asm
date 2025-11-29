40e3d54d002686e85bb876a0fc715f75940744ae
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-29 22:48:48 +0800

    Implementing the reading of input in the jjrc minimart menu

--- POS.asm
+++ POS.asm
@@ -27,7 +27,7 @@ include C:\masm32\include\masm32rt.inc
                   db "                               \/        \/          \/        \/         \/     \/              ",13,10,0
 
     ; ==== Minimart Option ====
-    minimartOption db "========= JJRC Minimart =========",13,10
+    minimartOption db 13,10,"========= JJRC Minimart =========",13,10
                    db "1. Inventory",13,10
                    db "2. Summary",13,10
                    db "3. POS", 13,10, 13,10
